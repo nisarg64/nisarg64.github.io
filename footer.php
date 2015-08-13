db4ade0ae415119160ea3e80ea327cf0b518a69a
Nisarg Gandhi <ngandhi@hortonworks.com>
2015-08-13 16:32:11 -0700

    TEMPLATE

---
+++ footer.php
@@ -0,0 +1,15 @@
+			
+			<?php themezee_footer_before(); // hook before #footer ?>
+			<div id="footer">
+				<?php 
+					$options = get_option('themezee_options');
+					if ( isset($options['themeZee_general_footer']) and $options['themeZee_general_footer'] <> "" ) { 
+						echo wp_kses_post($options['themeZee_general_footer']); } 
+				?>
+				<div class="credit_link"><?php themezee_credit_link(); ?></div>
+				<div class="clear"></div>
+			</div>
+			<?php themezee_footer_after(); // hook after #footer ?>
+			
+		</div><!-- end #contentwrap -->
+	</div><!-- end #wrap -->
